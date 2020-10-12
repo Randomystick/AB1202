67c9404abc1ab9ddf108a22b53295128197fc5b1
Randomystick <bryankhleow@gmail.com>
2020-10-12 19:37:14 +0800

    tut9

--- test.R
+++ test.R
@@ -1,34 +1,6 @@
-#####################
-###### PART C #######
-#####################
-
-# Calculate expected number of seats booked
-p = dbinom(size = 11,prob = 0.8,x = 11:0)
-bookingNumbers = c(11:0)
-EX = sum(p*bookingNumbers)
-#EX = 8.8
-
-# Calculate probability of an overbooked flight
-pOverbook = dbinom(size=11, prob=0.8, x=11)
-#pOverbook is 0.08589935
-
-EProfit = 12000*EX - 30000*pOverbook
-#EProfit = 103023
-
-#####################
-###### PART D #######
-#####################
-
-# Calculate expected number of seats booked
-p2 = dbinom(size = 10,prob = 0.8,x = 10:0)
-bookingNumbers2 = c(10:0)
-EX2 = sum(p2*bookingNumbers2)
-#EX2 = 8
-
-EProfit2 = 12000*EX2
-#EProfit2 = 96000
-
-#####################
-###### PART E #######
-#####################
-
+pop_sd = 70
+hypo_mean = 115
+X_bar = mean(ChickWeight$weight)
+n = length(ChickWeight$weight)
+pValue = 2*(1-pnorm(X_bar,hypo_mean, pop_sd/sqrt(n)))
+pValue
---
+++ tut9.R
@@ -0,0 +1,79 @@
+##################
+###### QN 4 ######
+##################
+
+pop_mean = 100 
+n = 16 
+X_bar = 95 
+pop_sd = 10 
+Zscore = (X_bar - pop_mean)/(pop_sd/sqrt(n))
+Zscore
+
+2*pnorm(Zscore, 0,1)
+
+##################
+###### QN 5 ######
+##################
+
+pop_mean = 45
+n = 36
+X_bar = 47
+pop_sd = 8.5
+Zscore = (X_bar-pop_mean)/(pop_sd/sqrt(n))
+Zscore
+
+1-pnorm(Zscore, 0,1)
+
+##################
+###### QN 6 ######
+##################
+
+n = 35
+pop_mean = 24
+X_bar = 16.4
+sample_sd = 7.3
+Tscore = (X_bar - pop_mean) / (sample_sd/sqrt(n))
+Tscore
+
+pt(Tscore, n-1)
+
+##################
+###### QN 7 ######
+##################
+
+#View(EuStockMarkets)
+pop_mean = 1620
+n = 30
+X_bar = mean(EuStockMarkets[1:30, "DAX"])
+sample_sd = sd(EuStockMarkets[1:30, "DAX"])
+Tscore = (X_bar - pop_mean) / (sample_sd / sqrt(n))
+Tscore
+2*(1-pt(Tscore,n-1))
+
+
+samplee = c(EuStockMarkets[1:30, "DAX"],EuStockMarkets[361:391, "DAX"])
+n = length(samplee)
+pop_mean = 1620
+X_bar=mean(samplee)
+sample_sd = sd(samplee)
+Tscore = (X_bar - pop_mean)/(sample_sd/sqrt(n))
+Tscore
+
+2*pt(Tscore, n-1)
+
+##################
+###### QN 8 ######
+##################
+
+n = 50
+pop_mean = 5
+X_bar = 4.6
+sample_sd = 0.7
+Tscore = (X_bar-pop_mean)/(sample_sd/sqrt(n))
+Tscore
+
+2*pt(Tscore, n-1)
+
+lowerLimit = X_bar-qt(.975, n-1)*(sample_sd/sqrt(n))
+upperLimit = X_bar+qt(.975, n-2)*(sample_sd/sqrt(n))
+c(lowerLimit, upperLimit)
